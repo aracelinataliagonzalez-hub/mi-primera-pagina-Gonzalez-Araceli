de6e5a1f79212320f92cdac7b297c37f253374f3
Araceli Gonzalez <aracelinataliagonzalez@gmsil.com>
2025-11-11 19:18:33 -0300

    creamos el modelo

--- inicio/views.py
+++ inicio/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 def inicio(request):
-    return render(request, inicio.html)
+    return render(request, 'inicio.html')
 
 def pedido(request):
-    return render(request, pedido.html)
+    return render(request, 'pedido.html')
 
 # Create your views here.
+
